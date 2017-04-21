9ce3f6a0dc8f8f70282f350ad7de882e267091ef
Carlos Mata <carlos.a.mata01@utrgv.edu>
2017-04-21 17:24:39 -0500

    added a menu bar, still needs functionalities

--- TabsProject/src/tabsproject/TabsProject.java
+++ TabsProject/src/tabsproject/TabsProject.java
@@ -3,7 +3,10 @@ package tabsproject;
 import GUIs.maingui;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -17,8 +20,23 @@ public class TabsProject extends Application {
     @Override
     public void start(Stage primaryStage) {
         maingui hitGUI = new maingui();
-        
-        Scene scene = new Scene(hitGUI.getRootNode());
+        Scene scene = new Scene(new VBox(), 400, 350);
+        scene.setFill(Color.OLDLACE);
+	
+	//Menu Bar
+        MenuBar menuBar = new MenuBar();
+ 
+        //File section: needs an exit option to exit the gui/program
+        Menu menuFile = new Menu("File");
+        //Edit section: ...
+        Menu menuEdit = new Menu("Edit");
+        //View section: alternate way of traversing gui tabs (probably using a checkmenu to toggle the options)
+        Menu menuView = new Menu("View");
+	//add the setions to the menu bar
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+ 
+ 
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar,hitGUI.getRootNode());
         primaryStage.setMaximized(true);
         primaryStage.setTitle("VaqChat");
         primaryStage.setScene(scene);
