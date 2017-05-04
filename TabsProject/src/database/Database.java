57ce8b408c1848465f48f2fa49b1e5e2d1936f61
JodyWhitis <jody.whitis01@utrgv.edu>
2017-05-03 22:44:50 -0500

    Added registerUser method to database class

--- TabsProject/src/database/Database.java
+++ TabsProject/src/database/Database.java
@@ -3,6 +3,8 @@ package database;
 import javafx.application.Platform;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  *
@@ -10,23 +12,38 @@ import java.sql.DriverManager;
  */
 public class Database {
 
-	//after database "vaqchat" is created, make the connection
-	
+	/*
+	after database "vaqchat" with 
+	table "profiles" is created, make the connection
+	 */
 	private Connection myconnection = null;
 	private String url = "jdbc:mysql://localhost:3306/vaqchat";
 	private String username = "root";
 	private String password = "";
-	
 
 	public Database() {
 	}
 
-	public Database(String url, String username,String password) {
+	
+	/***
+	 * Connecting to the database
+	 * @param url
+	 * @param username
+	 * @param password 
+	 */
+	public Database(String url, String username, String password) {
 		this.url = url;
 		this.username = username;
 		establishConnection(url, username, password);
 	}
 
+	
+	/***
+	 * Method to set database url, user, and password to make the connection
+	 * @param url
+	 * @param username
+	 * @param password 
+	 */
 	public void establishConnection(String url, String username, String password) {
 		this.url = url;
 		this.username = username;
@@ -42,8 +59,49 @@ public class Database {
 		}
 	}
 
-    
+	/***
+	 * Method to register users to database with input from profileTab
+	 * @param user
+	 * @param password
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @param webUrl
+	 * @param ID 
+	 */
+	public void registerUser(String user, String password, String firstName, String lastName, String email, String webUrl, String ID) {
 
-	
-	
-    }
+		String SQLString = "";
+
+		Statement mystatment = null;
+		ResultSet myRS = null;
+
+		try {
+			mystatment = myconnection.createStatement();
+			SQLString = "INSERT INTO vaqchat.profiles (username,passwords,firstName,lastName,email,weburl,id) VALUES"
+				+ "(\'" + user + "\'," + "\'" + password + "\'," + "\'" + firstName + "\',"
+				+ "\'" + lastName + "\',"
+				+ "\'" + email + "\',"
+				+ "\'" + webUrl + "\',"
+				+ "\'" + ID + "\'"
+				+ ");";
+
+			mystatment.executeLargeUpdate(SQLString);
+
+			if (myRS.isBeforeFirst()) {
+				//currentUser = new User();
+			}
+			while (myRS.next()) {
+				//System.out.println("id is:       " + myRS.getInt("iduser"));
+				System.out.println("Your name: " + myRS.getString("firstName") + " " + myRS.getString("lastName") + "\nYour email: " + myRS.getString("email"));
+
+				System.out.println("first name is:         " + myRS.getString("firstName"));
+				System.out.println("last name is:         " + myRS.getString("lastName"));
+			}
+		} catch (Exception e) {
+			System.out.println("stack:" + e.getMessage());
+		}
+
+	}
+
+}
