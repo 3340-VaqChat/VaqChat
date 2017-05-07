450eb36a3cc933859845eb5c398c698cc00423f8
JodyWhitis <jody.whitis01@utrgv.edu>
2017-05-06 19:51:56 -0500

    Course button adds to database from 1st column, for testing themodify button can display courses on console for the matching studentID field.

--- TabsProject/src/database/Database.java
+++ TabsProject/src/database/Database.java
@@ -108,6 +108,102 @@ public class Database {
 	}
 
 	
+	/***
+	 * Method for adding courses only with correct studentID is entered
+	 * @param courseID
+	 * @param prefix
+	 * @param courseNumber
+	 * @param section
+	 * @param start
+	 * @param end
+	 * @param location
+	 * @param instructor
+	 * @param prereq
+	 * @param studentID 
+	 */
+	public void addCourses(String courseID,String prefix, String courseNumber, String section, String start,String end,String location,String instructor,String prereq,String studentID) {
+		establishConnection(url, username, password);
+
+		String SQLString = "";
+
+		Statement mystatment = null;
+		ResultSet myRS = null;
+
+		try {
+			mystatment = myconnection.createStatement();
+			SQLString = "INSERT INTO vaqchat.courses (id,prefix,number,section,start,end,location,instructor,prereq,profileid) VALUES"
+				+ "(\'" + courseID + "\'," + "\'" + prefix + "\'," +  "\'" + courseNumber + "\',"
+				+ "\'" + section + "\',"
+				+ "\'" + start + "\',"
+				+ "\'" + end + "\',"
+				+ "\'" + location + "\',"
+				+ "\'" + instructor + "\',"
+				+ "\'" + prereq + "\',"
+				+ "\'" + studentID + "\'"
+				+ ");";
+
+			mystatment.executeLargeUpdate(SQLString);
+			System.out.println("NEW Courses for studentID: " + myRS.getString("profileid") + " ADDED");
+
+			if (myRS.isBeforeFirst()) {
+				//currentUser = new User();
+			}
+			while (myRS.next()) {
+				//System.out.println("id is:       " + myRS.getInt("iduser"));
+				System.out.println("Your Course: " + myRS.getString("prefix") + " " + myRS.getString("number") );
+
+			}
+		} catch (Exception e) {
+			
+			System.out.println("stack:" + e.getMessage());
+		}
+
+	}
+	
+	
+	
+	/***
+	 * Method to take studentID and return courses with matching studentID as an arraylist
+	 * @param studentID
+	 * @return courseList
+	 */
+	public ArrayList<String> displayCourses(String studentID) {
+
+		ArrayList courseList = new ArrayList<String>();
+		establishConnection(url, username, password);
+
+		String SQLString = "";
+
+		Statement mystatment = null;
+		ResultSet myRS = null;
+
+		try {
+			mystatment = myconnection.createStatement();
+			SQLString = "SELECT profiles.id,profiles.firstName,profiles.lastName,courses.prefix,courses.id FROM vaqchat.courses INNER JOIN vaqchat.profiles ON profiles.id=courses.profileid WHERE profiles.id LIKE" + "\'"+ studentID + "\';";
+			myRS = mystatment.executeQuery(SQLString);
+
+			if (myRS.isBeforeFirst()) {
+				//currentUser = new User();
+			}
+			while (myRS.next()) {
+				String addCourses = myRS.getString("prefix");
+				courseList.add(addCourses);
+
+				//System.out.println("id is:       " + myRS.getInt("iduser"));
+				System.out.println(myRS.getString("profiles.firstName") + " " + myRS.getString("profiles.lastName") + "'s courses: " 
+					+ myRS.getString("courses.prefix")+" - "+myRS.getString("courses.id"));
+			}
+		} catch (Exception e) {
+
+			System.out.println("stack:" + e.getMessage());
+		}
+
+		System.out.println("\nARRAY OF COURSES: " + courseList);
+
+		return courseList;
+
+	}
+	
 	
 	/***
 	 * Method to return the list of all emails registered in the database
@@ -148,4 +244,10 @@ public class Database {
 		return emailList;
 
 	}
+	
+	
+	
+	
+	
+	
 }
