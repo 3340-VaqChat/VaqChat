b17a7b3826a1b2e584a8e0ce8e6caf7711d87c15
Carlos Mata <carlos.a.mata01@utrgv.edu>
2017-05-08 21:12:49 -0500

    cleaned up the menu and added a sign out button under File: still needs functionality

--- TabsProject/src/tabsproject/TabsProject.java
+++ TabsProject/src/tabsproject/TabsProject.java
@@ -78,25 +78,19 @@ public class TabsProject extends Application {
 
 		// File menu - new, save, exit
 		Menu fileMenu = new Menu("File");
-		MenuItem newMenuItem = new MenuItem("New");
-		MenuItem openMenuItem = new MenuItem("Open");
-		openMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+		MenuItem outMenuItem = new MenuItem("Sign-out");
+		outMenuItem.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent args) {
-				FileChooser fileChooser = new FileChooser();
-				FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("All Files", "*.*");
-				fileChooser.getExtensionFilters().add(extFilter);
-				File file = fileChooser.showOpenDialog(primaryStage);
-				System.out.println(file);
+				
 			}
 		});
-		MenuItem saveMenuItem = new MenuItem("Save");
+		
 		MenuItem exitMenuItem = new MenuItem("Exit");
 		exitMenuItem.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
 		exitMenuItem.setOnAction(actionEvent -> Platform.exit());
 
-		fileMenu.getItems().addAll(newMenuItem, openMenuItem, saveMenuItem,
-			new SeparatorMenuItem(), exitMenuItem);
+		fileMenu.getItems().addAll(outMenuItem,new SeparatorMenuItem(), exitMenuItem);
 		//Edit section...
 		Menu editMenu = new Menu("Edit");
 		MenuItem copyMenuItem = new MenuItem("Copy");
@@ -108,65 +102,11 @@ public class TabsProject extends Application {
 		MenuItem selectallMenuItem = new MenuItem("Select all");
 		editMenu.getItems().add(selectallMenuItem);
 
-		//View menu...
-		Menu menuView = new Menu("View");
-		chatstab pane = new chatstab();
-
-		ToggleGroup tGroup = new ToggleGroup();
-		RadioMenuItem mode1 = new RadioMenuItem("Login");
-		mode1.setToggleGroup(tGroup);
-		mode1.setSelected(true);
-		mode1.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-
-			}
-		});
-		RadioMenuItem mode2 = new RadioMenuItem("Chat");
-		mode2.setToggleGroup(tGroup);
-		mode2.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-
-			}
-		});
-		RadioMenuItem mode3 = new RadioMenuItem("Profiles");
-		mode3.setToggleGroup(tGroup);
-		mode3.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-
-			}
-		});
-		RadioMenuItem mode4 = new RadioMenuItem("Courses");
-		mode4.setToggleGroup(tGroup);
-		mode4.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				//pane.tabPane.getSelectionModel().selectFirst();
-			}
-		});
-		RadioMenuItem mode5 = new RadioMenuItem("About");
-		mode5.setToggleGroup(tGroup);
-		mode5.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-
-			}
-		});
-		menuView.getItems().addAll(mode1, mode2, mode3, mode4, mode5);
-
-		//Settings menu...
-		Menu settingsMenu = new Menu("Settings");
-		MenuItem settingsItem1 = new MenuItem("Chat options");
-		settingsMenu.getItems().add(settingsItem1);
-		MenuItem settingsItem2 = new MenuItem("Tabs options");
-		settingsMenu.getItems().add(settingsItem2);
 		//Help menu...
 		Menu helpMenu = new Menu("Help");
-
-		helpMenu.getItems().addAll(new SeparatorMenuItem());
 		//Tutorial menu inside the help menu
-		Menu tutorialMenu = new Menu("Tutorial");
-		tutorialMenu.getItems().addAll(
-			new MenuItem("JavaFX"));
-
-		tutorialMenu.setOnAction(new EventHandler<ActionEvent>() {
+		MenuItem javaMenu = new MenuItem("JavaFX");
+		javaMenu.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				try {
@@ -178,10 +118,22 @@ public class TabsProject extends Application {
 				}
 			}
 		});
+		MenuItem utMenu = new MenuItem("UTRGV");
+		utMenu.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				try {
+					Desktop.getDesktop().browse(new URI("http://www.utrgv.edu/en-us/"));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		helpMenu.getItems().addAll(javaMenu, utMenu);
 
-		helpMenu.getItems().add(tutorialMenu);
-
-		menuBar.getMenus().addAll(fileMenu, editMenu, menuView, settingsMenu, helpMenu);
+		menuBar.getMenus().addAll(fileMenu, editMenu,helpMenu);
 
 		((VBox) scene.getRoot()).getChildren().addAll(menuBar, hitGUI.getRootNode());
 		primaryStage.setMaximized(true);
